7ef828e0d32513cbe43ddb44c032dc9350decd48
nur518 <99117333+nur518@users.noreply.github.com>
2022-03-09 12:56:43 +0600

    read using fast-csv

--- index.js
+++ index.js
@@ -7,3 +7,15 @@ fs.createReadStream(path.join(__dirname, 'demo.csv'))
   .pipe(csvParser({}))
   .on('data', (data) => result.push(data))
   .on('end', () => console.log(result))
+
+
+// using fast
+// const fastcsv = require("fast-csv");
+// fs.createReadStream("public/blank-template.csv")
+// .pipe(fastcsv.parse({ headers: true })) //{ headers: true } for object view
+// .on("error", (error) => console.error(error))
+// .on("data", (item) => {result.push(item)})
+// .on("end", (rowCount) => {
+//   console.log(`Parsed ${rowCount} rows`)
+//   console.log(result);
+// })
